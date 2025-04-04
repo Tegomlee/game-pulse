b3942be6dd05f764c6d34dc7009157519116ff17
Bryan <46428583+Tegomlee@users.noreply.github.com>
2025-04-03 20:14:53 -0400

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bryan/Repos/Projects/C-CPP/game-pulse
+# Build directory: /Users/bryan/Repos/Projects/C-CPP/game-pulse/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[GamePulseTests]=] "gamepulse_tests")
+set_tests_properties([=[GamePulseTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/bryan/Repos/Projects/C-CPP/game-pulse/CMakeLists.txt;32;add_test;/Users/bryan/Repos/Projects/C-CPP/game-pulse/CMakeLists.txt;0;")
+subdirs("_deps/unity-build")
